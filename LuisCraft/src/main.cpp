abd4d8aeb7abe09acbe53c280e0fbf298abfbd10
Luis Amado <lamadoalvarez@gmail.com>
2023-10-30 22:07:26 -0600

    Include GLFW

--- LuisCraft/src/main.cpp
+++ LuisCraft/src/main.cpp
@@ -1,8 +1,20 @@
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cin.get();
+
+    GLFWwindow* window;
+    glfwInit();
+
+    window = glfwCreateWindow(600, 480, "Hello, World!", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
 
     return 0;
 }
