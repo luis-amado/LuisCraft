1f140401f5836890d334216c908cee93c474facc
Luis Amado <lamadoalvarez@gmail.com>
2023-10-31 00:24:17 -0600

    First triangle

--- LuisCraft/src/main.cpp
+++ LuisCraft/src/main.cpp
@@ -1,20 +1,60 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 
 int main() {
 
     GLFWwindow* window;
-    glfwInit();
 
-    window = glfwCreateWindow(600, 480, "Hello, World!", nullptr, nullptr);
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    /* Initialize GLAD */
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+    glClearColor(0.1f, 0.7f, 0.4f, 1.0f);
+
+    float verts[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+
+    unsigned int vao, vbo;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+
+    /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        /* Swap front and back buffers */
         glfwSwapBuffers(window);
+
+        /* Poll for and process events */
         glfwPollEvents();
     }
 
     glfwTerminate();
-
     return 0;
 }
